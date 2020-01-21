da419c22d324c321a50a61c2cd8dfe189dd66f0d
anikett-rpx <anikett_c@rpxcorp.com>
2020-01-21 23:19:38 +0530

    Implemented remember me and terms functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+       session[:user_id] = @user.id
       redirect_to root_url, notice: "You have signed up"
     else
       render :new
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :terms)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_token :auth_token
 
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6, maximum: 20 }, on: :create
+  validates :terms, acceptance: true
+
+  attr_accessor :terms
 end
